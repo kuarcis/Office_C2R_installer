13790af916e4eee5d5612ed018fddfcaea524b1e
kuarcis <kuarcis@gmail.com>
2018-02-24 23:41:22 +0800

    加入專案檔案。

---
+++ Office_C2R_installer/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 組件的一般資訊是由下列的屬性集控制。
+' 變更這些屬性的值即可修改組件的相關
+' 資訊。
+
+' 檢閱組件屬性的值
+
+<Assembly: AssemblyTitle("Office_C2R_installer")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Office_C2R_installer")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+<Assembly: Guid("7f6f78fd-64ec-4a9a-a44e-c5239b991290")>
+
+' 組件的版本資訊由下列四個值所組成: 
+'
+'      主要版本
+'      次要版本
+'      組建編號
+'      修訂編號
+'
+' 您可以指定所有的值，或將組建編號或修訂編號設為預設值
+' 指定為預設值: 
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
